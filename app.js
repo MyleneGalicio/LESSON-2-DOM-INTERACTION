a2a49f8be74fc036a4009fea747202f47836d7e7
Mylene N. Galicio <79599031+MyleneGalicio@users.noreply.github.com>
2021-10-31 09:06:48 -0400

    Activity 2_Event Binding

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-const app = Vue.createApp({
-  data(){
-    return{
-      images: [
-      'https://picsum.photos/600/200',
-      'https://picsum.photos/600/200',
-      'https://picsum.photos/600/200'],
-      
-      selectedImage: null,
-  
-      name: '',
-      age: 20,
-    };
-  },
-
-  methods: {
-      Age(){
-        return this.age + 5;
-      },
-      randomNum(){
-        const randomNumber = Math.random();
-        if(randomNumber >0 && randomNumber <1){
-          return randomNumber;
+new Vue({
+    el: '#container',
+    data: {
+            output1: '',
+            output2: ''
+    },
+      methods: {
+        showAlert() {
+            alert('Alert Initialization Complete!')
+        },
+        setOutput1(event) {
+            this.output1 = event.target.value
+        },
+        setOutput2(event) {
+            this.output2 = event.target.value
         }
-      },
-      randomItem (items) {
-        return items[Math.floor(Math.random()*items.length)];
-      }
-      },
-      created() {
-        this.selectedImage = this.randomItem(this.images)
-      },
+    }
 })
-app.mount('#assignment');
-
